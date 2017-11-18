37e605674e33cbb191f72d50d03bc3e2787c06c9
vidhi <vidhi@Lenovo-PC>
2017-11-18 16:18:23 -0500

    Updated PalindromeHighlight class to support palindromecheck, isLengthGreaterThanThree and capitalizeWord methods

--- FileVisitors/src/fileVisitors/visitor/PalindromeHighlight.java
+++ FileVisitors/src/fileVisitors/visitor/PalindromeHighlight.java
@@ -1,16 +1,50 @@
 package fileVisitors.visitor;
 
 import element.ElementI;
+import element.TreeBuilder;
+import fileVisitors.myTree.MyTree;
+import fileVisitors.util.FileProcessor;
+import fileVisitors.util.InputProcessor;
+import fileVisitors.util.PalindromeCheck;
 
 /**
  *
- * @author anirtek
+ * @author vidhi
  */
 public class PalindromeHighlight implements VisitorI {
 
+    FileProcessor fp = null;
+    InputProcessor ip = null;
+    TreeBuilder builder = null;
+    MyTree tree = null;
+    PalindromeCheck check = null;
+
     @Override
     public void visit(ElementI element) {
+        String line = "";
+        check = new PalindromeCheck();
         
+        while ((line = fp.readLine()) != null) {
+            String[] inputParse = ip.processInput(line);
+            for (int i = 0; i < inputParse.length; i++) {
+                if(check.isPalindrome(inputParse[i])) {
+                    if(isLengthGreaterThanThree(inputParse[i])){
+                        capitalizeWord(inputParse[i]);
+                    }
+                }
+            }
+        }
     }
     
+    boolean isLengthGreaterThanThree(String nodeWord){
+	if(nodeWord.length() > 3){
+		return true;
+        }
+        return false;
+    }
+    
+    void capitalizeWord(String nodeWord){   
+	nodeWord.toUpperCase();
+    }
+
 }
