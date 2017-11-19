79286300ccbe94b23ab961ca64285dee67c747a1
vidhi <vidhi@Lenovo-PC>
2017-11-18 19:50:39 -0500

    PrintTree.java: Added code to traverse and printTree alphanumerically

--- FileVisitors/src/fileVisitors/visitor/PrintTree.java
+++ FileVisitors/src/fileVisitors/visitor/PrintTree.java
@@ -1,10 +1,37 @@
 package fileVisitors.visitor;
 
+import element.ElementI;
+import fileVisitors.myTree.MyTree;
+import fileVisitors.myTree.Node;
+import java.util.ArrayList;
+
 /**
  *
  * @author anirtek
  */
-public class PrintTree {
-    
-}
+public class PrintTree implements VisitorI {
 
+    MyTree tree = null;
+
+    @Override
+    public void visit(ElementI element) {
+        printNodes(tree.root);//??
+    }
+
+    public void printNodes(Node current_node) {
+        if (current_node == null) {
+            return;
+        }
+        printNodes(current_node.getLeft());
+        System.out.println(current_node.getWord());
+        printNodes(current_node.getRight());
+    }
+
+    public MyTree getTree() {
+        return tree;
+    }
+
+    public void setTree(MyTree tree) {
+        this.tree = tree;
+    }
+}
