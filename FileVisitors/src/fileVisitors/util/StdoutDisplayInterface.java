ecc1acfe6d77aab397fe69e0dbe18f2395adc4ac
vidhi <vidhi@Lenovo-PC>
2017-11-19 23:01:09 -0500

    StdoutDisplayInterface.java: Added interface to support writeToStdout method

---
+++ FileVisitors/src/fileVisitors/util/StdoutDisplayInterface.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fileVisitors.util;
+
+/**
+ *
+ * @author vidhi
+ */
+public interface StdoutDisplayInterface {
+        public void writeToScreen();
+}
