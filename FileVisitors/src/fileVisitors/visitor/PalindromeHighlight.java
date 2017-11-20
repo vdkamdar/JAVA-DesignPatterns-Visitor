2d2f23289a7d72518b6df0ec4fe1e03ded7b58c5
vidhi <vidhi@Lenovo-PC>
2017-11-19 23:02:11 -0500

    PalindromeHighlight.java: Removed unnecessary getters and setters

--- FileVisitors/src/fileVisitors/visitor/PalindromeHighlight.java
+++ FileVisitors/src/fileVisitors/visitor/PalindromeHighlight.java
@@ -1,6 +1,5 @@
 package fileVisitors.visitor;
 
-import element.ElementI;
 import fileVisitors.myTree.MyTree;
 import fileVisitors.myTree.Node;
 import fileVisitors.util.PalindromeCheck;
@@ -17,9 +16,9 @@ public class PalindromeHighlight implements VisitorI {
     //getTree from PopulateVisitor here or driver?
     
     @Override
-    public void visit(ElementI element) {
+    public void visit(MyTree tree) {
         check = new PalindromeCheck();
-        traverseTree(tree.root);//??
+        traverseTree(tree.getRoot());//??
     }
 
     void traverseTree(Node current_node) {
@@ -43,13 +42,5 @@ public class PalindromeHighlight implements VisitorI {
 
     void capitalizeWord(Node current_node) {
         current_node.setWord(current_node.getWord().toUpperCase());
-    }
-
-    public MyTree getTree(){
-        return tree;
-    }
-    
-    public void setTree(MyTree tree){
-        this.tree = tree;
     }
 }
