adcc265b382973ff1b405cabca6710b8f63402e7
vidhi <vidhi@Lenovo-PC>
2017-11-19 22:59:34 -0500

    FileDisplayInterface.java: Added the interface to support writeToFile method

---
+++ FileVisitors/src/fileVisitors/util/FileDisplayInterface.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fileVisitors.util;
+
+import fileVisitors.myTree.MyTree;
+
+/**
+ *
+ * @author vidhi
+ */
+public interface FileDisplayInterface {
+    public void writeSchedulesToFile(MyTree tree);
+}
