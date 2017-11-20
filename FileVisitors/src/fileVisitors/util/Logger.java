02b67c96cc2b6b6ecdba7139422f5168816ff0a5
Aniruddha Tekade <atekade1@binghamton.edu>
2017-11-20 01:21:38 -0500

    Logger level and actions are defined. Detailed explanation in the Readme.txt

--- FileVisitors/src/fileVisitors/util/Logger.java
+++ FileVisitors/src/fileVisitors/util/Logger.java
@@ -1,27 +1,22 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package fileVisitors.util;
 
 /**
- *
- * @author vidhi
+ * @version 4.0
+ * @author Vidhi Kamdar & Aniruddha Tekade Submitted on November 22nd, 2017.
  */
 public class Logger {
 
     /*DEBUG_VALUE=4 [Print to stdout everytime a constructor is called]
-      DEBUG_VALUE=3 [Print to stdout everytime a thread's run() method is called]
-      DEBUG_VALUE=2 [Print to stdout everytime an insertion is attempted]
-      DEBUG_VALUE=1 [Print to stdout everytime a deletion is attempted]
+      DEBUG_VALUE=3 [Print to stdout everytime a accept() method is called]
+      DEBUG_VALUE=2 [Print to stdout everytime a visit() is attempted]
+      DEBUG_VALUE=1 [Print to stdout everytime a palindromeCheck is performed]
       DEBUG_VALUE=0 [No output should be printed from the application to stdout. However, it doesn't affect what's get written to the output file." ]
      */
     public static enum DebugLevel {
         RELEASE,
-        WORD_DELETION,
-        WORD_INSERTION,
-        THREAD_RUN,
+        PALINDROME_CHECK,
+        VISITOR_ACTIVE,
+        ELEMENT_ACCEPT,
         CONSTRUCTOR
     };
 
@@ -33,13 +28,13 @@ public class Logger {
                 debugLevel = DebugLevel.CONSTRUCTOR;
                 break;
             case 3:
-                debugLevel = DebugLevel.THREAD_RUN;
+                debugLevel = DebugLevel.ELEMENT_ACCEPT;
                 break;
             case 2:
-                debugLevel = DebugLevel.WORD_INSERTION;
+                debugLevel = DebugLevel.VISITOR_ACTIVE;
                 break;
             case 1:
-                debugLevel = DebugLevel.WORD_DELETION;
+                debugLevel = DebugLevel.PALINDROME_CHECK;
                 break;
             case 0:
                 debugLevel = DebugLevel.RELEASE;
